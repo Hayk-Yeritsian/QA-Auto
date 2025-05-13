5a92eecd47d610cdc4f8fe291c6d70b23543303a
macbook <yeritsianhayk@gmail.com>
2025-05-13 20:18:31 +0400

    Lessaon2

---
+++ src/main/java/am/picsartacademy/lessan2/Lessaon2.java
@@ -0,0 +1,12 @@
+package am.picsartacademy.lessan2;
+
+public class Lessaon2 {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        for (int i = 1; i < array.length; i++) {
+            array[i] = 2;
+            System.out.println(i);
+        }
+    }
+}
+
