a9708240cfe811c2e71a74c8351f85a768be5c4b
macbook <yeritsianhayk@gmail.com>
2025-05-14 19:38:44 +0400

    Homework3

--- src/main/java/am/picsartacademy/lessan2/Homework.java
+++
@@ -1,7 +0,0 @@
-package am.picsartacademy.lessan2;
-
-public class Homework {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/am/picsartacademy/lessan2/Homework1.java
+++ src/main/java/am/picsartacademy/lessan2/Homework1.java
@@ -1,4 +1,7 @@
 package am.picsartacademy.lessan2;
 
 public class Homework1 {
+    public static void main(String[] args) {
+
+    }
 }
